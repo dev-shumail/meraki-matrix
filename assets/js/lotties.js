7c895da94cd5498faa765f13ac9587c7e3597f88
dev-shumail <shumail.mushtaq@otoz.ai>
2025-03-25 10:25:06 +0500

    updated: lottieflies animation

---
+++ assets/js/lotties.js
@@ -0,0 +1,7 @@
+lottie.loadAnimation({
+  container: document.getElementById("web-development-lottie"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "../img/features/Animation-1742809848557.json",
+});
